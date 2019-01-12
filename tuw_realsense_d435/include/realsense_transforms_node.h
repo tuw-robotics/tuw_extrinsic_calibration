85bd1aa104635577f067f861f132ac43371e43a7
Felix König <felix.koenig@protonmail.com>
2019-01-12 13:36:17 +0100

    Adjusted publisher topic name

--- tuw_realsense_d435/include/realsense_transforms_node.h
+++ tuw_realsense_d435/include/realsense_transforms_node.h
@@ -10,10 +10,10 @@
 
 namespace tuw
 {
-  
+
   class RealSenseTransformsNode
   {
-  
+
   public:
     struct CameraInternalExtrinsics
     {
@@ -21,38 +21,39 @@ namespace tuw
       Eigen::Matrix4d t_origin_leftrgb_;
       Eigen::Matrix4d t_leftrgb_origin_;
     };
-    
+
     struct CameraExternalExtrinsics
     {
     public:
       Eigen::Matrix4d t_base_cam_origin_;
       bool usable;
     };
-    
+
     RealSenseTransformsNode();
-    
+
     bool readExternalCalibrationFromFile();
-    
+
     void readInternalCalibrationFromXML();
-    
+
     void doTransform( const Eigen::Matrix4d &tf_base_cam_optical_frame );
-    
+
     void publish();
-    
+
     void callbackTransform( const geometry_msgs::TransformConstPtr &tf_base_cam_origin );
-  
+
   private:
     ros::Subscriber sub_transform_;
     ros::Publisher pub_transform_;
+    std::string publisher_topic_;
     tf::TransformBroadcaster tf_broadcaster_;
     ros::NodeHandle nh_;
     std::string calib_file_;
     std::string external_calib_file_;
     CameraInternalExtrinsics camera_internal_;
     CameraExternalExtrinsics camera_external_;
-    
+
   };
-  
+
 }
 
 #endif
--- tuw_realsense_d435/src/realsense_transforms_node.cpp
+++ tuw_realsense_d435/src/realsense_transforms_node.cpp
@@ -9,63 +9,64 @@ using namespace tuw;
 
 typedef boost::tokenizer<boost::escaped_list_separator<char>> Tokenizer;
 
-RealSenseTransformsNode::RealSenseTransformsNode() : nh_( "~" )
+RealSenseTransformsNode::RealSenseTransformsNode() : nh_("~")
 {
   camera_external_.usable = false;
-  nh_.param<std::string>( "calib_file", calib_file_, "" );
-  nh_.param<std::string>( "external_calib_file", external_calib_file_, "" );
-  
-  sub_transform_ = nh_.subscribe( "tf_base_cam_optical_frame",
-                                  1000,
-                                  &RealSenseTransformsNode::callbackTransform,
-                                  this );
-  
-  pub_transform_ = nh_.advertise<geometry_msgs::Transform>( "calib", 10 );
+  nh_.param<std::string>("calib_file", calib_file_, "");
+  nh_.param<std::string>("external_calib_file", external_calib_file_, "");
+  nh_.param<std::string>("publisher_topic", publisher_topic_, "/r0/realsense");
+
+  sub_transform_ = nh_.subscribe("tf_base_cam_optical_frame",
+                                 1000,
+                                 &RealSenseTransformsNode::callbackTransform,
+                                 this);
+
+  pub_transform_ = nh_.advertise<geometry_msgs::Transform>(publisher_topic_, 10);
 }
 
 bool RealSenseTransformsNode::readExternalCalibrationFromFile()
 {
   using namespace boost;
   using namespace std;
-  
-  if ( external_calib_file_ == std::string( "" ))
+
+  if ( external_calib_file_ == std::string(""))
   {
     return false;
   }
-  
-  ifstream in( external_calib_file_.c_str());
+
+  ifstream in(external_calib_file_.c_str());
   if ( !in.is_open())
   {
     std::cout << "Object read: File path is wrong" << std::endl;
   }
-  
+
   string line;
   vector<vector<string>> parsed_csv;
-  
-  while ( getline( in, line ))
+
+  while ( getline(in, line))
   {
     if ( line[0] == '#' )
     {
       continue; // allows simple comments
     }
-    parsed_csv.push_back( vector<string>());
-    Tokenizer tok( line );
+    parsed_csv.push_back(vector<string>());
+    Tokenizer tok(line);
 
 //    if (std::distance(tok.begin(),tok.end()) != nr_line_parameters)
 //      throw runtime_error("number of entries in csv file wrong. Must provide position (3 variables) plus shape variables (4) for each door.");
-    
-    for_each( tok.begin(), tok.end(), [&parsed_csv]( string elem )
+
+    for_each(tok.begin(), tok.end(), [ &parsed_csv ]( string elem )
     {
-      parsed_csv.back().push_back( elem );
-    } );
+      parsed_csv.back().push_back(elem);
+    });
   }
-  
-  assert( parsed_csv.size() == 4 );
-  assert( parsed_csv[0].size() == 4 );
-  assert( parsed_csv[1].size() == 4 );
-  assert( parsed_csv[2].size() == 4 );
-  assert( parsed_csv[3].size() == 4 );
-  
+
+  assert(parsed_csv.size() == 4);
+  assert(parsed_csv[0].size() == 4);
+  assert(parsed_csv[1].size() == 4);
+  assert(parsed_csv[2].size() == 4);
+  assert(parsed_csv[3].size() == 4);
+
   Eigen::Matrix4d tf_base_cam_optical_frame;
   for ( int r = 0; r < 4; ++r )
   {
@@ -73,60 +74,60 @@ bool RealSenseTransformsNode::readExternalCalibrationFromFile()
     {
       try
       {
-        
-        tf_base_cam_optical_frame( r, c ) = boost::lexical_cast<double>( parsed_csv[r][c] );
-        
-      } catch (const boost::bad_lexical_cast &)
+
+        tf_base_cam_optical_frame(r, c) = boost::lexical_cast<double>(parsed_csv[r][c]);
+
+      } catch ( const boost::bad_lexical_cast & )
       {
-        ROS_INFO( "could not cast %s into double, check %s\n", parsed_csv[r][c].c_str(), external_calib_file_.c_str());
+        ROS_INFO("could not cast %s into double, check %s\n", parsed_csv[r][c].c_str(), external_calib_file_.c_str());
       }
     }
   }
-  
+
   std::cout << "tf_base_cam_optical_frame " << tf_base_cam_optical_frame << std::endl;
-  
-  doTransform( tf_base_cam_optical_frame );
-  
+
+  doTransform(tf_base_cam_optical_frame);
+
   return true;
 }
 
 void RealSenseTransformsNode::readInternalCalibrationFromXML()
 {
-  Eigen::Vector3d trans = Eigen::Vector3d( 0, 0, 0 );
+  Eigen::Vector3d trans = Eigen::Vector3d(0, 0, 0);
   Eigen::Matrix3d rot;
   rot.setIdentity();
-  
+
   TiXmlDocument xml;
-  if ( xml.LoadFile( calib_file_ ))
+  if ( xml.LoadFile(calib_file_))
   {
-    auto config = xml.FirstChild( "Config" );
+    auto config = xml.FirstChild("Config");
     if ( config )
     {
-      TiXmlElement *param = config->FirstChild( "param" )->ToElement();
+      TiXmlElement *param = config->FirstChild("param")->ToElement();
       TiXmlElement *sibl = param;
       while ( sibl )
       {
-        if ( std::string( sibl->Attribute( "name" )) == std::string( "TranslationLeftRGB" ))
+        if ( std::string(sibl->Attribute("name")) == std::string("TranslationLeftRGB"))
         {
-          auto value = sibl->FirstChild( "value" );
+          auto value = sibl->FirstChild("value");
           if ( value )
           {
             try
             {
-              trans.x() = boost::lexical_cast<double>( value->ToElement()->GetText());
+              trans.x() = boost::lexical_cast<double>(value->ToElement()->GetText());
               value = value->NextSibling();
-              trans.y() = boost::lexical_cast<double>( value->ToElement()->GetText());
+              trans.y() = boost::lexical_cast<double>(value->ToElement()->GetText());
               value = value->NextSibling();
-              trans.z() = boost::lexical_cast<double>( value->ToElement()->GetText());
-            } catch (boost::bad_lexical_cast const &)
+              trans.z() = boost::lexical_cast<double>(value->ToElement()->GetText());
+            } catch ( boost::bad_lexical_cast const & )
             {
-              ROS_ERROR( "lexical cast: cannot cast double (calib xml TranslationLeftRGB)" );
+              ROS_ERROR("lexical cast: cannot cast double (calib xml TranslationLeftRGB)");
             }
           }
-        } else if ( std::string( sibl->Attribute( "name" )) == std::string( "RotationLeftRGB" ))
+        } else if ( std::string(sibl->Attribute("name")) == std::string("RotationLeftRGB"))
         {
-          auto vals = sibl->FirstChild( "value" );
-          
+          auto vals = sibl->FirstChild("value");
+
           if ( vals )
           {
             try
@@ -135,22 +136,22 @@ void RealSenseTransformsNode::readInternalCalibrationFromXML()
               {
                 for ( int c = 0; c < rot.cols(); ++c )
                 {
-                  rot( r, c ) = boost::lexical_cast<double>( vals->ToElement()->GetText());
+                  rot(r, c) = boost::lexical_cast<double>(vals->ToElement()->GetText());
                   vals = vals->NextSibling();
                 }
               }
-            } catch (std::runtime_error const &)
+            } catch ( std::runtime_error const & )
             {
-              ROS_ERROR( "parsing rotation: too few <value></value> arguments provided" );
+              ROS_ERROR("parsing rotation: too few <value></value> arguments provided");
             }
-            catch (boost::bad_lexical_cast const &)
+            catch ( boost::bad_lexical_cast const & )
             {
-              ROS_ERROR( "lexical cast: cannot cast double (calib xml RotationLeftRGB)" );
+              ROS_ERROR("lexical cast: cannot cast double (calib xml RotationLeftRGB)");
             }
           }
         }
-        
-        auto toElementCastable = sibl->NextSibling( "param" );
+
+        auto toElementCastable = sibl->NextSibling("param");
         if ( toElementCastable )
         {
           sibl = toElementCastable->ToElement();
@@ -158,17 +159,17 @@ void RealSenseTransformsNode::readInternalCalibrationFromXML()
         {
           sibl = nullptr;
         }
-        
+
       } //end while
-      
-      std::stringstream sstr( "" );
+
+      std::stringstream sstr("");
       sstr << "Parsed file for camera parameters\n";
       sstr << "translation: \n";
       sstr << trans << "\n";
       sstr << "rotation: \n";
       sstr << rot << "\n";
-      ROS_INFO( "%s\n", sstr.str().c_str());
-      
+      ROS_INFO("%s\n", sstr.str().c_str());
+
       //converting stuff to meters from cm;
       trans = trans / 100.0;
       camera_internal_.t_leftrgb_origin_.setIdentity();
@@ -176,7 +177,7 @@ void RealSenseTransformsNode::readInternalCalibrationFromXML()
       camera_internal_.t_leftrgb_origin_.topRightCorner<3, 1>() = trans;
       std::cout << "tf_leftrgb_origin " << std::endl;
       std::cout << camera_internal_.t_leftrgb_origin_ << std::endl;
-      
+
       //invert stuff without relying on less accurate Eigen functions
       Eigen::Matrix3d rot_t = rot.transpose();
       Eigen::Vector3d trans_t = -trans;
@@ -188,7 +189,7 @@ void RealSenseTransformsNode::readInternalCalibrationFromXML()
     }
   } else
   {
-    ROS_ERROR( "Failed to load config file" );
+    ROS_ERROR("Failed to load config file");
   }
 }
 
@@ -196,15 +197,15 @@ void RealSenseTransformsNode::callbackTransform( const geometry_msgs::TransformC
 {
   auto &q = tf_base_cam_optical_center->rotation;
   auto &t = tf_base_cam_optical_center->translation;
-  Eigen::Quaterniond qe = Eigen::Quaterniond( q.w, q.y, q.z, q.x );
-  Eigen::Vector3d te = Eigen::Vector3d( t.x, t.y, t.z );
-  
+  Eigen::Quaterniond qe = Eigen::Quaterniond(q.w, q.y, q.z, q.x);
+  Eigen::Vector3d te = Eigen::Vector3d(t.x, t.y, t.z);
+
   Eigen::Matrix4d tf_base_cam_optical_frame;
   tf_base_cam_optical_frame.setIdentity();
   tf_base_cam_optical_frame.topLeftCorner<3, 3>() = qe.toRotationMatrix();
   tf_base_cam_optical_frame.topRightCorner<3, 1>() = te;
-  
-  doTransform( tf_base_cam_optical_frame );
+
+  doTransform(tf_base_cam_optical_frame);
 }
 
 void RealSenseTransformsNode::doTransform( const Eigen::Matrix4d &tf_base_cam_optical_frame )
@@ -219,39 +220,39 @@ void RealSenseTransformsNode::publish()
   tf_.header.stamp = ros::Time::now();
   tf_.header.frame_id = "r0/base_link";
   tf_.child_frame_id = "r0/mount_camera_tof";
-  
+
   auto m_rot = camera_external_.t_base_cam_origin_.topLeftCorner<3, 3>();
   Eigen::Vector3d v_trans = camera_external_.t_base_cam_origin_.topRightCorner<3, 1>();
-  
-  auto quat = Eigen::Quaterniond( m_rot );
+
+  auto quat = Eigen::Quaterniond(m_rot);
   tf_.transform.rotation.x = quat.x();
   tf_.transform.rotation.y = quat.y();
   tf_.transform.rotation.z = quat.z();
   tf_.transform.rotation.w = quat.w();
-  
+
   tf_.transform.translation.x = v_trans.x();
   tf_.transform.translation.y = v_trans.y();
   tf_.transform.translation.z = v_trans.z();
-  
-  tf_broadcaster_.sendTransform( tf_ );
+
+  tf_broadcaster_.sendTransform(tf_);
 }
 
 int main( int argc, char **argv )
 {
-  ros::init( argc, argv, "realsense_transforms_node" );
+  ros::init(argc, argv, "realsense_transforms_node");
   RealSenseTransformsNode node;
   node.readInternalCalibrationFromXML();
   node.readExternalCalibrationFromFile();
-  
-  ros::Rate rate( 5 );
+
+  ros::Rate rate(5);
   while ( ros::ok())
   {
-    
+
     ros::spinOnce();
     rate.sleep();
-    
+
     node.publish();
-    
+
   }
   return 0;
 }
